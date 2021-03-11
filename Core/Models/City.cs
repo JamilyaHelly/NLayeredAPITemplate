6ae2c7c76d1df02df75ad0ea2acd5af8205b63c9
Jamilya Halyyeva <jamilyahalyyeva@outlook.com>
2021-03-11 21:46:27 +0100

    Fixes: Properties are changed

--- Core/Models/City.cs
+++ Core/Models/City.cs
@@ -2,8 +2,8 @@ namespace Core.Models
 {
     public class City
     {
-        public int CityId { get; set; }
-        public string CityName { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
         public int PostCode { get; set; }
         public int CountryId { get; set; }
         public virtual Country  Country { get; set; }
