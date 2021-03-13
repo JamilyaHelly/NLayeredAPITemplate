f31cbc298ac765cdaf0d26467f6d46cbb19d8774
Jamilya Halyyeva <jamilyahalyyeva@outlook.com>
2021-03-13 01:12:25 +0100

    Fixes: DbContext is wired

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.UnitOfWorks;
+using Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +29,16 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+             services.AddDbContext <AppDbContext>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("NLayerConStr"), o =>
+                {
+                    o.MigrationsAssembly("Data");
+                });
+            });
 
+            services.AddScoped<IUnitOfWork,UnitOfWork>();
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
